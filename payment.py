32593fd37e2db4201776ebe146a92385895d9232
python3126 <latha18819@gmail.com>
2026-09-16 08:10:04 +0000

    Add payment program

---
+++ payment.py
@@ -0,0 +1,6 @@
+amount=int(input("Enter the payment amount: "))
+
+if amount > 0:
+    print("Payment of", amount, "is successful.")
+else:
+    print("Invalid payment amount. Please enter a positive value.")
